99411f56c779d1f2ab86831db73f768ffbe2c063
Sangyong Bae <kiros33@gmail.com>
2026-06-28 11:47:45 +0900

    fix(cask): 불필요한 verified 파라미터 제거

--- Casks/nexa-markdown-viewer.rb
+++ Casks/nexa-markdown-viewer.rb
@@ -2,8 +2,7 @@ cask "nexa-markdown-viewer" do
   version "0.2.1"
   sha256 "08a0f9965e5660f60877b883ea6294919c0185ec6d2284a2152386f9a125c725"
 
-  url "https://github.com/kiros33/sosomlab-nexa-viewer/releases/download/v#{version}/NexaMarkdownViewer_#{version}_universal.dmg",
-      verified: "github.com/kiros33/sosomlab-nexa-viewer/"
+  url "https://github.com/kiros33/sosomlab-nexa-viewer/releases/download/v#{version}/NexaMarkdownViewer_#{version}_universal.dmg"
   name "Nexa Markdown Viewer"
   desc "Markdown viewer built with Tauri"
   homepage "https://github.com/kiros33/sosomlab-nexa-viewer"
